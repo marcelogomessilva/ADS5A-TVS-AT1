511b10873af71b266c3e2ee56c115ab6862cb104
marcelogomessilva <165839475+marcelogomessilva@users.noreply.github.com>
2024-04-12 18:14:25 -0300

    marcelo gomes

--- recebeDados.test.js
+++ recebeDados.test.js
@@ -2,9 +2,9 @@
 const dadosEstudante = require('./dadosEstudante');
 
 describe('Teste da função recebeDados', () => {
-  test('Verifica se a função processa corretamente o nome "Mellinda Gates" e o RA "228697"', () => {
-    const nome = "Mellinda Gates";
-    const RA = "228697";
+  test('Verifica se a função processa corretamente o nome "Marcelo Gomes" e o RA "033127"', () => {
+    const nome = "Marcelo Gomes";
+    const RA = "033127";
     const esperado = `Dados recebidos - Nome: ${nome}, RA: ${RA}`;
 
     expect(dadosEstudante(nome, RA)).toBe(esperado);
